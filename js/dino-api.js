f06f7e64536c688556de54657fed81060594a646
Kyle Wolfson and Sam Durfey <sam@>
2016-05-26 11:39:59 -0700

    Refactor code to have appropriate click listeners.

--- js/dino-api.js
+++ js/dino-api.js
@@ -1,43 +1,14 @@
 var encodeName = require('./../js/encodeDino.js').encode;
 
-exports.getDinosaur = function() {
-    var output = "";
+exports.getDinosaur = function(listener) {
 
-    $.ajax('http://dinoipsum.herokuapp.com/api/?format=text&paragraphs=1&words=1')
-      .then(function(response){
-        output = response.slice(0, response.length -3);
-        console.log(output);
-        $('#encodedDiv').text(encodeName(output));
-        dinoText = output;
-
-        //letter guess click listener.
-        $('#letterSubmit').click(function(){
-          var guess = $("#letter").val();
-          debugger;
-          guess = guess.slice(0,1);
-          console.log(guess);
-          if (dinoText.toLowerCase().includes(guess)){
-            console.log("Success");
-            var code = $('#encodedDiv').html();
-            for (var i = 0; i < dinoText.length; i++) {
-              if (dinoText[i].toLowerCase() === guess.toLowerCase()) {
-                code = setCharAt(code, i, dinoText[i]);
-              }
-            }
-            console.log(code);
-            $('#encodedDiv').text(code);
-
-          } else {
-            console.log("failure");
-            errors -= 1;
-            console.log("You have " + errors + " mistakes left.");
-            if (errors === 0) {
-              alert("You lose. Press Start to create a new game.");
-            }
-          }
-          $("#letter").val('');
-        });
-      });
+  $.ajax('http://dinoipsum.herokuapp.com/api/?format=text&paragraphs=1&words=1').then(function(response){
+    var dinoText = response.slice(0, response.length -3);
+    console.log(dinoText);
+    $('#encodedDiv').text(encodeName(dinoText));
+    // return dinoText;
+    listener(dinoText);
+  });
 };
   exports.fillContainer = function(html) {
     $('#outputTestDiv').text(html);
--- js/dino-interface.js
+++ js/dino-interface.js
@@ -2,24 +2,52 @@ var getDinosaur = require('./../js/dino-api.js').getDinosaur;
 var oops = require('./../js/dino-api.js').oops;
 var fillContainer = require('./../js/dino-api.js').fillContainer;
 var encodeName = require('./../js/encodeDino.js').encode;
-var dinosaur;
-
-
-
+// var dinosaur;
 
 function setCharAt(str,index,chr) {
-    if(index > str.length-1) return str;
-    return str.substr(0,index) + chr + str.substr(index+1);
-}
+  if(index > str.length-1) return str;
+  return str.substr(0,index) + chr + str.substr(index+1);
+};
+
+var listener = function(dinoText) {
+  $('#letterSubmit').click(function(){
+    var guess = $("#letter").val();
+    // debugger;
+    guess = guess.slice(0,1);
+    console.log(guess);
+    if (dinoText.toLowerCase().includes(guess)){
+      console.log("Success");
+      var code = $('#encodedDiv').html();
+      for (var i = 0; i < dinoText.length; i++) {
+        if (dinoText[i].toLowerCase() === guess.toLowerCase()) {
+          code = setCharAt(code, i, dinoText[i]);
+        }
+      }
+      console.log(code);
+      $('#encodedDiv').text(code);
+
+    } else {
+      console.log("failure");
+      errors -= 1;
+      console.log("You have " + errors + " mistakes left.");
+      if (errors === 0) {
+        $('#errorDiv').text("You lose. Press Start to create a new game.");
+      } else {
+        $('#errorDiv').text("You can make " + errors + " more errors before you lose.")
+      }
+    }
+    $("#letter").val('');
+  });
+};
 
 $(document).ready(function() {
-  //var dinoText = 'default';
-var errors;
-  $('#gameStart').click(function() {
-    getDinosaur();
-    errors = 10;
-  });
 
-  // the click listener for submitting a letter guess has been moved to dino-api.js
-  // pardon our dust
+  var errors;
+  $('#gameStart').click(function() {
+    $('#letterSubmit').off();
+    getDinosaur(listener);
+    errors = 10;
+    $('#errorDiv').text("You can make " + errors + " more errors before you lose.");
+
+  });
 });
